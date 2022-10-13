7134b81f27371dd4ee1e27f11475ee51fd4e302c
Tejas Protim Neog <92169576+tpncoder@users.noreply.github.com>
2022-10-13 20:29:17 +0530

    Update essentials.js

--- modules/essentials.js
+++ modules/essentials.js
@@ -1,6 +1,7 @@
 import * as variables from "./variables.js"
 
 export async function display(value_1, value_2) {
-	variables.country.textContent = value_1;	
-	variables.temp.textContent = value_2;	
+	variables.country.textContent = value_1;
+	variables.str = obj.temp;	
+    	variables.temp.textContent = `${str}°C`;
 }

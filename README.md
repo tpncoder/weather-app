c1ef34bc97b348527b6531cf896e4a0475503117
Tejas Protim Neog <92169576+tpncoder@users.noreply.github.com>
2022-10-13 19:59:00 +0530

    upload root dir files

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # weather-app
-A weather app
---
+++ index.js
@@ -0,0 +1,80 @@
+import * as essentials from './modules/essentials.js';
+import * as variables from './modules/variables.js'
+import * as highlights from './modules/highlights.js';
+// import {index} from './modules/variables.js';
+
+async function weatherIcon(object, img) {	
+	let icon = object.icon;	
+	let img_name = `./images/4th Set - Color/${icon}.svg`;
+	img.setAttribute('src', img_name);
+}
+
+var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+var today = new Date();		
+var day = today.getDay();
+var day_name = daylist[day];
+variables.childrenLeft[2].textContent = day_name;	
+
+
+async function setGrid(object) {
+	let children = variables.grid.children;
+	let child;
+	let day_temp;	
+
+
+	for(var index = 0; index <=5; index++){
+		children[index].style.height = "25vh";
+		children[index].style.width = "12vw";
+		children[index].style.borderRadius = "5%";
+		children[index].style.background = "rgba(254, 255, 255, .7)";
+		children[index].style.webkitBackdropFilter = "blur(10px)";
+		children[index].style.backdropFilter = "blur(10px)";
+
+		child = children[index].children;
+		day_temp = object.days[index];
+
+		child[0].textContent = day_name;
+		child[0].style.paddingBottom = "2vh";
+		child[0].style.paddingTop = "2vh";
+	
+		weatherIcon(object.days[index], child[1]);	
+
+		child[2].textContent = day_temp.temp;
+
+		if(typeof daylist[day + 1] === 'undefined') {
+			day = 0;
+			day_name = daylist[day];
+		} else {
+			day++;
+			day_name = daylist[day];
+		}
+	}	
+}
+
+async function main() {
+	var response =  await fetch(variables.ipUrl);
+	var ipResponse = await response.json();	
+
+	const ipJson = await JSON.stringify(eval(ipResponse));
+	const ipObj = await JSON.parse(ipJson);
+	var region = await ipObj.city;
+
+	let city = region;	
+
+	const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&include=days&key=${variables.weatherApiKey}&contentType=json`;
+
+	response = await fetch(url);
+	let weather = await response.json();
+	let json = JSON.stringify(eval(weather));
+	let obj = JSON.parse(json);
+
+	let resolvedAddress = obj.resolvedAddress;
+	let today = obj.days[0];
+
+	weatherIcon(today, variables.image);	
+	essentials.display(resolvedAddress, variables.temp_str);
+	setGrid(obj);
+	highlights.todayInfo(today);	
+}
+
+main();

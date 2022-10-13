173d915426722de29ae1c2fb977395477bfbfcff
Tejas Protim Neog <92169576+tpncoder@users.noreply.github.com>
2022-10-13 20:02:52 +0530

    Create variables.js

---
+++ modules/variables.js
@@ -0,0 +1,13 @@
+export const country = document.getElementById("country");
+export const temp = document.getElementById("temp");
+export const childrenLeft = document.getElementById("left-box").children;
+export const image = childrenLeft[0];
+export const grid = document.getElementById("week");
+export const highlights = document.getElementById("info_today")
+
+export const ApiKey = 'adecda7f7a6d463e930d0ab1e1324547';
+export const ipUrl = `https://ipgeolocation.abstractapi.com/v1/?api_key=${ApiKey}`;
+
+export const weatherApiKey = `P2CAZSS2V37YBZ34BVX4HQHCY`;
+
+export let temp_str;
